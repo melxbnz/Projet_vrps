548fa9908eeed563e3b49a43cae9eaf9a8dbfc7f
OlivierTchall <151485488+OlivierTchall@users.noreply.github.com>
2025-11-11 12:39:33 +0100

    Update alns.py

--- src/alns.py
+++ src/alns.py
@@ -1,1 +1,9 @@
 # OLIVIER
+import numpy as np
+import random
+import math
+
+
+#DECLARATION ET DEFINITION DES STRUCTURES DE DONNEES
+class Instance:
+  """ C'estlaclase represenant l'instance d nore pobleme de VRPTW
